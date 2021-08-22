6e68f55ced09a4b909e0c6c82fb031fd1e631fed
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-22 15:13:26 +0100

    subscribing and dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,16 +3,24 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1,
+        count: state.count + incrementBy,
       };
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
       };
     case "RESET":
       return {
         count: 0,
+      };
+    case "SET":
+      return {
+        count: 101,
       };
     default:
       return state;
@@ -20,20 +28,39 @@ const store = createStore((state = { count: 0 }, action) => {
 });
 
 // returns the current state
-console.log(store.getState());
+// console.log(store.getState());
 
+//renders when state updates
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+//unsubscribe to stop rendering
+// const unsubscribe = store.subscribe(() => {
+//   console.log(store.getState());
+// });
 // increment the count
 store.dispatch({
-  type: "INCREMENT",
+  type: "INCREMENT", //type is mandatory
+  incrementBy: 5,
 });
 store.dispatch({
   type: "INCREMENT",
 });
+
+// unsubscribe();
+
 store.dispatch({
   type: "RESET",
 });
 store.dispatch({
   type: "DECREMENT",
+  decrementBy: 10,
 });
-
-console.log(store.getState());
+store.dispatch({
+  type: "DECREMENT",
+});
+store.dispatch({
+  type: "SET",
+  count: 101,
+});
